602525c4eb1fd05216b00fc9b8eeeb21ea27f0ff
Azpeitia42 <a358430@uach.mx>
2024-11-19 11:41:39 -0700

    Apoco

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { NewsComponent } from './news/news.component';
+import { TeamsComponent } from './teams/teams.component';
+import { PlayersComponent } from './players/players.component';
+import { ScheduleComponent } from './schedule/schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: NewsComponent },
+  { path: 'equipos', component: TeamsComponent },
+  { path: 'jugadores', component: PlayersComponent },
+  { path: 'calendario', component: ScheduleComponent },
+  { path: '**', redirectTo: '' }, // Redirección a Noticias por defecto
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule {}
+
